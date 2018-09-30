6346bb3090d1e916eb84dc713b73309793ef27d0
Abdulhakim Desuki <Abdulhakim Desuki@LOLGIA>
2018-09-30 15:30:26 +0200

    hakim increment iteraion of i

--- Overview/src/OperatorDemo.java
+++ Overview/src/OperatorDemo.java
@@ -2,7 +2,7 @@
 public class OperatorDemo {
 	public static void main(String[] args) {
 
-   for (int i=1;i<=6;++i){
+   for (int i=1;i<=15;++i){
 	   for  (int j=1;j<=i;j++ ) {
 			System.out.print(j + " ");
 		}
