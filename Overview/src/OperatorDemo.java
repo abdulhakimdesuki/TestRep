2d4f00739d2d5cf0d368b0a4bf81d1499cc6206a
Abdulhakim Desuki <Abdulhakim Desuki@LOLGIA>
2018-10-01 14:57:09 +0200

    break and continue loop

--- Overview/src/OperatorDemo.java
+++ Overview/src/OperatorDemo.java
@@ -1,14 +1,29 @@
+import javax.swing.text.html.parser.Parser;
 
 public class OperatorDemo {
 	public static void main(String[] args) {
-
-   for (int i=1;i<=15;++i){
-	   for  (int j=1;j<=i;j++ ) {
+    for (int i=1;i<=6;++i){
+  	    for  (int j=1;j<=i;j++ ) {
 			System.out.print(j + " ");
-		}
-	   System.out.println();
+		 }
+	     System.out.println();
    }
 
+      System.out.println("--------------------");
+     for (int i=65;i<=67;++i){
+	      for  (int j=65;j<=i;j++ ) {
+	  		System.out.print((char)j + " ");
+	    	}
+	      System.out.println();
+     }
 		
+     System.out.println("--------------------");
+     for (int i = 0; i < 10; i++) {
+    	 if (i==7) {
+		  continue;	
+		}
+   		System.out.println(i);
+   	}
+     
    }
   }
