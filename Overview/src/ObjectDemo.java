2342d17fb9227f08661db53c345b81e5401700bc
Abdulhakim Desuki <Abdulhakim Desuki@LOLGIA>
2018-10-02 19:15:16 +0200

    Casio

--- Overview/src/ObjectDemo.java
+++ Overview/src/ObjectDemo.java
@@ -18,16 +18,30 @@ class Calc {
 	}
 }
 
+class Casio {
+	
+	public Casio(){
+	}
+	
+	public void add(int i, int j){
+		System.out.println(i + j);
+	}
+}
+
 public class ObjectDemo {
 
 	public static void main(String[] args) {
-		Calc obj; // this is not object it is a reference to object
+		
+		Casio add= new Casio();
+		
+	}
+	/*	Calc obj; // this is not object it is a reference to object
 		obj=new Calc(5,6);  // This is an Object which a clocate the memory  - Object knows something and does something
     
 	//	obj.num1 = 3;
 	//	obj.num2 = 15;
 		obj.perform();
        System.out.println(" Num1 = "+obj.num1+" Num2= "+ obj.num2 +" ="+obj.result);
-	}
+	}  */
 
 }
